fe1b698fb08445be6e6113790d825101437d4400
Huzaifa Khan <m.huzaifakhan2004@gmail.com>
2024-05-20 13:06:30 +0500

    working on services component

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,15 +1,19 @@
+import Footer from "./Components/Footer/Footer";
 import HomeComponent from "./Components/Home/HomeComponent";
 import NavbarComponent from "./Components/Navbar/NavbarComponent";
 import Projects from "./Components/Projects/Projects";
+import Services from "./Components/Services/Services";
 import Testimonials from './Components/Testimonials/Testimonials'
 
 export default function Home() {
   return (
-    <div className="dark text-foreground bg-background pb-[100px]">
+    <div className="dark text-foreground bg-background">
       <NavbarComponent />
       <HomeComponent />
       <Projects />
+      <Services />
       <Testimonials />
+      <Footer />
     </div>
   );
 }
