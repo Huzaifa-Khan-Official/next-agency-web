cb417e714abab1c851ab88e84d64ec0c084cc8c9
Huzaifa Khan <m.huzaifakhan2004@gmail.com>
2024-05-21 21:13:27 +0500

    animation on services component done

--- src/app/Components/Services/Services.jsx
+++ src/app/Components/Services/Services.jsx
@@ -174,7 +174,12 @@ const ServiceCard = ({ icon, title, details }) => {
     return (
         <>
             <div className="w-full px-4 md:w-1/2 lg:w-1/3">
-                <div className="mb-9 rounded-[20px] bg-white px-3 py-4 shadow-2 hover:shadow-lg dark:bg-dark-2 md:px-7 xl:px-10">
+                <motion.div className="mb-9 rounded-[20px] bg-white px-3 py-4 shadow-2 hover:shadow-lg dark:bg-dark-2 md:px-7 xl:px-10"
+                    initial={{ opacity: 0, translateY: "50px" }}
+                    whileInView={{ opacity: 1, translateY: 0 }}
+                    viewport={{ once: true }}
+                    transition={{ duration: 1 }}
+                >
                     <div className="mb-8 flex h-[70px] w-[70px] items-center justify-center rounded-2xl bg-primary">
                         {icon}
                     </div>
@@ -182,7 +187,7 @@ const ServiceCard = ({ icon, title, details }) => {
                         {title}
                     </h4>
                     <p className="text-black">{details}</p>
-                </div>
+                </motion.div>
             </div>
         </>
     );
